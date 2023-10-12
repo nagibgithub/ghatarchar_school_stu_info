46a4896a1adaeee658b50382b10cce1c1116250a
Nagib Mahfuz <120543584+nagibgithub@users.noreply.github.com>
2023-10-12 19:31:14 +0600

    add logo

--- src/contents/NavBar.jsx
+++ src/contents/NavBar.jsx
@@ -11,8 +11,10 @@ const NavBar = () => {
 
     return (
         <div className="w-full">
-            <img src="https://drive.google.com/file/d/12JZ161jloy27J2Xl1a82I6SzxJVNnuwa/view" alt="" />
-            <h1 className="text-center py-2 mb-2 text-lg font-serif font-semibold text-blue-800">Ghatarchar Secondary School</h1>
+            <div className="flex flex-col items-center justify-center gap-2 pt-5">
+                <img className="w-20" src="https://raw.githubusercontent.com/nagibgithub/ghatarchar_school_stu_info/main/src/assets/132.jpg" alt="" />
+                <h1 className="text-center py-2 mb-2 text-lg font-serif font-semibold text-blue-800">Ghatarchar Secondary School</h1>
+            </div>
             <div className="flex justify-center">
                 {
                     navOptions.map((nav, index) => <NavLink onClick={() => sessionStorage.clear()} className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : "inactive"} key={index} to={nav.path}>{nav.element}</NavLink>)
