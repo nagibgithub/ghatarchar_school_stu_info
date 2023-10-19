012991e120abc0e4abcd1dbfa9db2a16ea019ef7
Nagib Mahfuz <120543584+nagibgithub@users.noreply.github.com>
2023-10-19 20:21:06 +0600

    for backup

--- src/pages/attendence/AttendenceDay.jsx
+++ src/pages/attendence/AttendenceDay.jsx
@@ -7,18 +7,56 @@ const AttendenceDay = () => {
     const dayId = useParams().id;
 
     const [dayData, setDayData] = useState({});
+    const [stuData, setStuData] = useState([]);
     const [loading, setLoading] = useState(true);
     useEffect(() => {
         setLoading(true);
         const url = `https://school-student-info-client.vercel.app/presents_day/${dayId}`;
         axios.get(url).then(res => { setDayData(res.data); setLoading(false); }).catch(err => console.log(err));
-        // fetch(url).then(res => res.json()).then(data => { setDayData(data); setLoading(false); }).catch(err => console.log(err.message));
     }, [dayId]);
-    loading ? console.log("loading...!") : console.log(dayData);
+
+    console.log(stuData);
+    const presentStu = dayData.present_Stu;
+    // const objData = { presentStu };
+    // console.log(objData);
+    useEffect(() => {
+        // setLoading(true);
+        const url = `http://localhost:5000/student_present_info`;
+        // axios.get(url, presentStu).then(res => { setStuData(res.data); setLoading(false); }).catch(err => console.log(err));
+        const unsub = fetch(url, {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({ stuName: "Nagib Mahfuz" })
+        })
+            .then(res => res.json())
+            .then(data => {
+                setStuData(data);
+            })
+            .catch(err => console.log(err));
+        return () => {
+            unsub
+        }
+    }, []);
+
+    console.log(presentStu);
+    // console.log(stuData);
+
+
 
     return (
-        <div>
-            to day
+        <div className="flex flex-col justify-center items-center">
+            {
+                loading ?
+                    <>
+                        <h1>Loading...!</h1>
+                    </>
+                    :
+                    <>
+                        <h1>Data paichi</h1>
+                    </>
+            }
         </div>
     );
 };
